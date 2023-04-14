8c012dee202650efa16a2a38a39b6aed42354a6e
chanmyeong <chanmyeongwoo@gmail.com>
2023-04-14 21:53:45 +0900

    Update 15552.java
    [빠른 A+B]: https://www.acmicpc.net/problem/15552
    
    BufferedReader + BufferedWriter

--- BAEKJOON/15552.java
+++ BAEKJOON/15552.java
@@ -56,6 +56,31 @@ public class Main {
 
 ====================================================================================================
 
+// split()
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int T = Integer.parseInt(br.readLine());
+        while(T-- > 0) {
+            String[] nums = br.readLine().split(" ");
+            int a = Integer.parseInt(nums[0]);
+            int b = Integer.parseInt(nums[1]);
+            bw.write(a+b+"\n");
+
+        }
+        bw.flush();
+        bw.close();
+    }
+}
+
+====================================================================================================
+
 // substring 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
