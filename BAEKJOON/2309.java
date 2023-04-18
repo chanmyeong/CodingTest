73573ef8fb9f1435e03b602f4fd9486a78007541
chanmyeong <chanmyeongwoo@gmail.com>
2023-04-18 11:16:01 +0900

    Update 2309.java
    [일곱 난쟁이]: https://www.acmicpc.net/problem/2309

--- BAEKJOON/2309.java
+++ BAEKJOON/2309.java
@@ -119,7 +119,7 @@ public class Main {
 }
 
 ====================================================================================================
-
+//O[N^2]
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -165,4 +165,36 @@ class Main
             if (h[i] > 0)
                 System.out.println(h[i]);
     }
-}  
+}
+
+====================================================================================================
+//O[N^2]
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args)  throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] a = new int[9];
+        int sum = 0;
+        for (int i = 0; i < 9; i++) {
+            a[i] = Integer.parseInt(br.readLine());
+            sum += a[i];
+        }
+        for(int i=0; i<9; i++){
+            for(int j=i+1; j<9; j++){
+                if(sum-a[i]-a[j]==100){
+                    a[i]=100;
+                    a[j]=100;
+                    Arrays.sort(a);
+                    for(int k=0; k<7; k++){
+                        System.out.println(a[k]);
+                    }
+                    return;
+                }
+            }
+        }
+    }
+}
