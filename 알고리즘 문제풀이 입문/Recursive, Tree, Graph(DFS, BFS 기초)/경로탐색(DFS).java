e4b82cbdf4ee5595329214ca1023dc608be85052
chanmyeong <chanmyeongwoo@gmail.com>
2023-05-17 04:22:29 +0900

    Create 경로탐색(DFS).java

---
+++ 알고리즘 문제풀이 입문/Recursive, Tree, Graph(DFS, BFS 기초)/경로탐색(DFS).java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int n, m, answer=0;
+    static int[][] graph;
+    static int[] ch;
+    public void DFS(int v) {
+        if(v==n) answer++;
+        else {
+            for(int i=1; i<=n; i++) {
+                if(graph[v][i]==1 && ch[i]==0) {
+                    ch[i]=1;
+                    DFS(i);
+                    ch[i]=0;
+                }
+            }
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        m = sc.nextInt();
+        Main T = new Main();
+        graph = new int[n+1][n+1];
+        ch=new int[n+1];
+        for(int i=0; i<m; i++) {
+            graph[sc.nextInt()][sc.nextInt()]=1;
+        }
+
+        ch[1]=1;
+        T.DFS(1);
+        System.out.println(answer);
+
+    }
+}
