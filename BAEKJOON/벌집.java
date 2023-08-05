44c826f45c14c5e4398fb71eaf03cd9200b674a8
chanmyeong <chanmyeongwoo@gmail.com>
2023-08-05 14:32:07 +0900

    Update 벌집.java
    [baekjoon]: 2292

--- BAEKJOON/벌집.java
+++ BAEKJOON/벌집.java
@@ -47,6 +47,28 @@ public class Main{
 import java.util.Scanner;
 
 public class Main {
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		int n = sc.nextInt();
+		int lastNum = 1;
+		int answer = 1;
+		int idx = 1;
+		int i =0;
+		while (lastNum < n) {
+			lastNum += 6 * idx;
+			answer++;
+			idx++;
+		}
+		System.out.println(answer);
+
+	}
+}
+====================================================================================================
+import java.util.Scanner;
+
+public class Main {
 	public static void main(String args[]) {
 		Scanner scan = new Scanner(System.in);
 		
