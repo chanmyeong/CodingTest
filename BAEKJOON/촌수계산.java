ae7aa06aaa7065bd45409a84db1e2f5af2bc32f7
chanmyeong <chanmyeongwoo@gmail.com>
2023-09-10 09:20:34 +0900

    Update 촌수계산.java
    [baekjoon]: 2644

--- BAEKJOON/촌수계산.java
+++ BAEKJOON/촌수계산.java
@@ -15,7 +15,87 @@ import java.io.*;
  * 그래프 BFS 탐색
  * 인접하면 촌수 출력 아니면 -1
  * 0촌도 존재한다
+ * 
+ * 예외 테스트 케이스 :
+ * cnt를 queue에서 꺼낼 때 증가시키고 큐에서 찾을 때 return 시키게 된다면
+ * 큐에서 찾을 때 찾고자 하는 정점이 첫번째 원소가 아닐 때 cnt값이 정답과 달라진다.
+ * BFS 시 각 정점마다의 촌수를 저장하는 자료구조가 필요해보인다.
+9
+2 3
+7
+1 3
+2 7
+2 8
+1 2
+2 9
+4 5
+4 6
  */
+// 틀렸던 나의 풀이
+public class Main {
+    static List<Integer>[] adjList;
+    static int N, M, A, B, answer = -1;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        A = Integer.parseInt(st.nextToken());
+        B = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(br.readLine());
+
+        adjList = new List[N + 1]; // 1부터 시작
+        boolean[] visited = new boolean[N + 1];
+        for (int i = 1; i < adjList.length; i++) {
+            adjList[i] = new ArrayList<>();
+        }
+
+        for (int i = 1; i <= M; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            int from = Integer.parseInt(st.nextToken());
+            int to = Integer.parseInt(st.nextToken());
+            adjList[from].add(to);
+            adjList[to].add(from);
+        }
+
+        for(List list : adjList) {
+            System.out.println(list);
+        }
+
+        BFS(A, visited);
+        System.out.println(answer);
+    }
+
+    private static void BFS(int start, boolean[] visited) { // A가 B와 촌수 관계인지
+        Queue<Integer> queue = new ArrayDeque<>();
+        visited[start] = true;
+        queue.offer(start);
+        int cnt = 0;
+        if (start == B) {
+            answer = cnt;
+            return;
+        }
+
+        while (!queue.isEmpty()) {
+            int current = queue.poll();
+            cnt++;
+
+            for (int i : adjList[current]) {
+                if (visited[i]) continue;
+                if (i == B) {
+                    answer = cnt;
+                    return;
+                }
+                visited[i] = true;
+                queue.offer(i);
+            }
+        }
+    }
+}
+====================================================================================================
+import java.util.*;
+import java.io.*;
+
 public class Main {
     static List<Integer>[] adjList;
     static int N, M, A, B, answer = -1;
