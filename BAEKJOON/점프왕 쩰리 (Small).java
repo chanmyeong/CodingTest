9f72bc08737632d0b374d0db97414bd63d8bed2c
chanmyeong <chanmyeongwoo@gmail.com>
2023-09-14 13:17:33 +0900

    Update 점프왕 쩰리 (Small).java
    [baekjoon]: 16173

--- BAEKJOON/점프왕 쩰리 (Small).java
+++ BAEKJOON/점프왕 쩰리 (Small).java
@@ -97,3 +97,100 @@ public class Main {
 		}
 	}
 }
+====================================================================================================
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+/**
+ * N*N(2<=N<=3) 정사각형 구역 내부만 존재 가능 
+ * 장외 죽음
+ * 쩰리 시작점(0,0) 패딩x
+ * 쩰리 이동 방향 => 오른쪽|아래
+ * 쩰리 도착점 (N-1)(N-1) 패딩x  -1이라 써있음
+ * 쩰리 이동 가능 칸 == 현재 밟고 있는 칸에 쓰인 수만큼. 초과/미만 x
+ * 					0<=x<=100
+ * output
+ * 도착점 도달 -> HaruHaru
+ * 도달x -> Hing 
+ *
+ * [문제해결 프로세스]
+ * bfs
+ * 1.이동 방향(오른쪽,아래) =>{0,1},{1,0}
+ * 		이동 = 이동방향 * 현재 밟고 있는 값
+			방문체크
+ * 2.경계 나가면 바로 종료, Hing 출력
+ * 3.현재 밝고 있는 칸이 0이면 바로 종료,Hing 출력
+ * 
+ *
+ */
+public class Main {
+
+	static int dn[][]= {{0,1},{1,0}};//우,하
+	static int N,map[][];
+	static boolean flag;
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N= Integer.parseInt(br.readLine());
+		map=new int[N][N];
+		for (int i = 0; i < N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < N; j++) {
+				map[i][j]=Integer.parseInt(st.nextToken());
+			}
+		}
+		bfs();
+		System.out.println(flag?"HaruHaru":"Hing");
+		
+
+
+	}
+	private static void bfs() {
+		boolean visited[][]=new boolean[N][N];
+		Queue<Node> q = new ArrayDeque<Node>();
+		q.offer(new Node(0,0));
+		while(!q.isEmpty()) {
+			Node cur = q.poll();
+			//방문체크
+			if(visited[cur.y][cur.x])continue;
+			visited[cur.y][cur.x]=true;
+			int move=map[cur.y][cur.x];//현재 밟고 있는 칸의 이동값
+
+			// 방문배열을 쓰지 않을 경우의 (small) -> 인덱스가 0일 경우를 고려해주어야 메모리 초과가 나지 않는다
+			if(move==0)return;//밟은 칸의 값이 0일 경우 종료
+			if(move==-1) {//목표지 도달
+				flag=true;
+				return;
+			}
+			
+			for (int i = 0; i < 2; i++) {//우,하 이동
+				//값만큼 이동
+				int dy=cur.y+dn[i][0]*move;
+				int dx=cur.x+dn[i][1]*move;
+				if(!inRange(dy, dx)&&!visited[dy][dx]) {
+				q.offer(new Node(dy,dx));
+				}
+			}
+		}
+	}
+	private static boolean inRange(int i, int j) {
+		if(i>=N || j>=N) return true;
+		return false;
+	}
+	static class Node{
+		int y,x;
+
+		public Node(int y, int x) {
+			super();
+			this.y = y;
+			this.x = x;
+		}
+
+	}
+
+}
