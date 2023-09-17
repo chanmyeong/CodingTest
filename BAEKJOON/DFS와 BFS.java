4a667a1485b15efbe6d1e2eac934168dacc9fc15
chanmyeong <chanmyeongwoo@gmail.com>
2023-09-17 23:03:19 +0900

    Update DFS와 BFS.java
    [baekjoon]: 1260

--- BAEKJOON/DFS와 BFS.java
+++ BAEKJOON/DFS와 BFS.java
@@ -1,3 +1,86 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Queue;
+import java.util.StringTokenizer;
+/**
+ * 방문할 수 있는 정점이 여러 개인 경우에는 정점 번호가 작은 것을 먼저 방문하고,
+ * 더 이상 방문할 수 있는 점이 없는 경우 종료
+ */
+public class Main {
+	static int N,M,V;
+	
+	static List<Integer>[] adjList;
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		V = Integer.parseInt(st.nextToken());
+		adjList = new List[N+1]; // List 컬렉션 구현, 인접리스트
+		boolean[] visited = new boolean[N+1]; // 1부터 시작
+		
+		for(int i=1; i<=N; i++) {
+			adjList[i] = new ArrayList<>();
+		}
+		
+		for(int i=1; i<=M; i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			adjList[a].add(b);
+			adjList[b].add(a);
+		}
+		
+//		for(List list : adjList) {
+//			System.out.println(list); // 출력 시 list와 배열(Arrays.toString)을 구분하자
+//		}
+		
+		for(int i=1; i<=N; i++) {
+			Collections.sort(adjList[i]); // 크기가 작은 정점부터 방문하기 위해 오름차순 정렬
+		}
+		
+		DFS(V, visited);
+		System.out.println();
+		Arrays.fill(visited, false);
+		BFS(V, visited);
+	}
+	private static void DFS(int current, boolean visited[]) {
+		visited[current] = true;
+		System.out.print(current+" ");
+		
+		for(int to : adjList[current]) {
+			if(!visited[to]) { 
+				DFS(to, visited);
+			}
+		}
+		
+	}
+	private static void BFS(int current, boolean visited[]) {
+		Queue<Integer> queue = new ArrayDeque<>();
+		visited[current] = true;
+		queue.offer(current);
+		
+		while(!queue.isEmpty()) {
+			int from = queue.poll();
+			System.out.print(from+" ");
+			
+			for(int to : adjList[from]) {
+				if(!visited[to]) {
+					visited[to] = true;
+					queue.offer(to);
+				}
+			}
+		}
+		
+	}
+}
+====================================================================================================
 import java.io.*;
 import java.util.*;
 
