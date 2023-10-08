c750cfaec985481e8df45338faafdf6b7a171b3c
chanmyeong <chanmyeongwoo@gmail.com>
2023-10-09 03:41:18 +0900

    Update 창고 다각형.java
    [baekjoon]: 2304

--- BAEKJOON/IM Essential/창고 다각형.java
+++ BAEKJOON/IM Essential/창고 다각형.java
@@ -26,6 +26,13 @@ public class Main {
       }
  
       Arrays.sort(col, (o1, o2) -> o1[0] - o2[0]); // 막대 정렬
+	   
+      Arrays.sort(col, new Comparator<int[]>() { // 막대 정렬2
+         @Override
+	 public int compare(int[] o1, int[] o2) {
+	    return o1[0]-o2[0]; // 첫번째 숫자 기준 오름차순
+	 }
+      });
  
       int area = 0;
       for(int i=0;i<N;){
